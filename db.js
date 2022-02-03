071414e968c4fcab4e55d6c6e6cb4d5549119f95
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2022-02-03 15:38:49 -0500

    adjusting db.js to return the require of sequelize.

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
+const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:'postgres',
+    dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
 });
 
