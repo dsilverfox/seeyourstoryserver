b2489fe93e5bbfba5b22bfa62ac4630b18bd0a5b
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2022-02-03 15:30:50 -0500

    Correcting db.js for Heroku deploy.

--- db.js
+++ db.js
@@ -1,4 +1,6 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:'postgres',
+    ssl: process.env.ENVIRONMENT === 'production'
+});
 
 module.exports = sequelize;
